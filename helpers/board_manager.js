c142224dbb9eb7ea6561aef7dec7e6a234894a77
torbastian <torbastianhansen@hotmail.com>
2021-11-04 09:40:47 +0100

    Update board_manager.js

--- helpers/board_manager.js
+++ helpers/board_manager.js
@@ -1,18 +1,40 @@
-const { GetBoardList } = require("./board_handler");
+const { GetBoardList, GetBoardAsOwner, GetBoardAsMember } = require("./board_handler");
 
 class BoardManager {
   constructor() {
-    this.boardSubscriptions = {};
+    this.boardListSubscriptions = {};
+    //Hvis en user er i board list subscription, send hver board opdatering til user
   }
 
   async subscribeToBoardList(subscriber, userId) {
     try {
-      console.log(userId);
+      //Hvis brugeren ikke er abbonneret, opret et abbonnoment
+      if (!this.boardListSubscriptions[userId]) {
+        this.boardListSubscriptions[userId] = {
+          subscriber: subscriber
+        }
+      }
 
-
+      this.sendBoardList(userId);
+      return true;
     } catch (error) {
-
+      return false;
     }
+  }
+
+  async sendBoardList(userId) {
+    if (!this.boardListSubscriptions[userId]) return;
+
+    const owned = await GetBoardAsOwner(userId);
+    const memberOf = await GetBoardAsMember(userId);
+
+    const listPackage = JSON.stringify({
+      time: Date.now(),
+      owned: owned,
+      memberOf: memberOf
+    });
+
+    this.boardListSubscriptions[userId].subscriber.send(listPackage);
   }
 }
 
