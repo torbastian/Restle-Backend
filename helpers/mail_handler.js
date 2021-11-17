6bce875bd28b211c5b0d0abbf84868eb297beb1e
Frederik <freddyagnb@gmail.com>
2021-11-17 13:44:30 +0100

    lots of stuff

--- helpers/mail_handler.js
+++ helpers/mail_handler.js
@@ -1,6 +1,6 @@
 var nodemailer = require('nodemailer');
 
-function SendEmail(reciver){
+function SendEmailReset(reciver, mailLink){
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -12,19 +12,16 @@ function SendEmail(reciver){
         from: 'restlehej@gmail.com',
         to: reciver,
         subject: 'Password reset',
-        text: 'That was easy!'
+        text: 'That was easy!' + mailLink
       };
       
       transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Email sent: ' + info.response);
         }
-      });
-    }
+    });
+}
     
-exports.SendEmail = SendEmail;
-
-
-
+exports.SendEmailReset = SendEmailReset;
--- helpers/websocket.js
+++ helpers/websocket.js
@@ -26,7 +26,8 @@ function startWebscoketServer(server) {
 
   //Når en bruger forbindes til serveren
   wss.on('connection', async (ws, req) => {
-
+    console.log("WS ", ws);
+    console.log("req ", req);
     //Verificer bruger token
     const userId = await VerifyUserToken(ws, req);
 
