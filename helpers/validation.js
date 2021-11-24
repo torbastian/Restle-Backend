d80da9756aba9bb39788a253292439752964b1e8
Frederik <freddyagnb@gmail.com>
2021-11-24 11:54:02 +0100

    password regex check

--- helpers/validation.js
+++ helpers/validation.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
 
 function registerValidation(data) {
+  console.log(data);
   const joiSchema = Joi.object({
     username: Joi.string().min(4).max(16).required(),
-    password: Joi.string().min(6).max(40).required(),
+    password: Joi.string().regex(new RegExp("^(?=.*[A-Z])(?=.*[!\"#¤%&()=?;:_*^'¨.,\\-\\/\\\\@£$€\\{\\[\\]\\}<>]).*$")).min(8).max(40).required(),
     first_name: Joi.string().min(1).max(40).required(),
     last_name: Joi.string().min(1).max(40).required(),
     email: Joi.string().email(),
@@ -16,7 +17,7 @@ function registerValidation(data) {
 function loginValidation(data) {
   const joiSchema = Joi.object({
     username: Joi.string().min(4).max(16).required(),
-    password: Joi.string().min(6).max(40).required()
+    password: Joi.string().regex(new RegExp("^(?=.*[A-Z])(?=.*[!\"#¤%&()=?;:_*^'¨.,\\-\\/\\\\@£$€\\{\\[\\]\\}<>]).*$")).min(8).max(40).required()
   }).unknown();
 
   return joiSchema.validate(data);
